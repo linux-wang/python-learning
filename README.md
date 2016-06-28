59a50306eb4f1a41b27c42be31c721d56fed471a
wang <wang@wswangdeMacBook-Air.local>
2016-06-28 22:42:59 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### python-learning
-1. flask
+1. hello.py中是flask练习的例子，主要是基本知识点什么的，不是一个完整的可以运行的程序
--- hello.py
+++ hello.py
@@ -1,27 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-	return 'hello'
-
-
+@app.route('/hello/')
 @app.route('/hello/<name>')
-def say_hello(name):
-    	return 'hello {username}'.format(username=name)
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
 @app.route('/post/<int:post_id>')
 def show_post_id(post_id):
-    	return 'post_id is %d' % post_id
+    return 'post_id is %d' % post_id
 
 
 # the difference of '/about/' and '/about'
 @app.route('/about/')
 def about():
-    	return 'about html'
+    return 'about html'
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.args.get('key', ''):   # request.form['username']
+            return 'login success'
+    else:
+        return 'wrong'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/upload' + secure_filename(f.file_name))
+
+
+@app.route('/get_user_name')
+def get_cookie_name():
+    user_name = request.cookies.get('username', '')
+    pass
+    # 使用 cookies.get(key) 来代替 cookies[key] ，
+    # 以避免当 cookie 不存在时引发 KeyError 。
+
+
+@app.route('/set_cookie_user_name')
+def set_user_name():
+    resp = make_response(render_template('hello.html', ))
+    resp.set_cookie('username', 'wang')
+    return resp
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
